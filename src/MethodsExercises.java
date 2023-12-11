6d644bccccc70104f5e9b4395b28a7507e23d1ed
mononoke813 <mononoke813@hotmail.com>
2023-12-11 10:02:52 -0600

    did stuff

---
+++ src/MethodsExercises.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+    static Scanner sc = new Scanner(System.in);
+
+//    public static int add(int a, int b) {
+//        return a + b;
+//    }
+//    public static int subtract(int a, int b) {
+//        return b - a;
+//    }
+//    public static int multiply(int a, int b) {
+//        return a * b;
+//    }
+//    public static int multiply(int a, int b) {
+//        int sum = 0;
+//        for(int i = 0; i < b; i++) {
+//            sum += a;
+//        }
+//        return sum;
+//    }
+//
+//    public static int divide(int a, int b) {
+//        return b / a;
+//    }
+//    public static int modulus(int a, int b) {
+//        return b % a;
+//    }
+
+
+//    public static int getInteger(int min, int max) {
+//
+//        System.out.println("Enter a number between 1 and 10: ");
+//        int userInput = sc.nextInt();
+//        if (userInput <= 0 || userInput > 10) {
+//            System.out.println("Negatory. Please enter a number between 1 and 10: ");
+//            return getInteger(min, max);
+//        } else {
+//            System.out.println("You entered the number: " + userInput);
+//            return userInput;
+//        }
+//    }
+
+    public static void calcFactorial() {
+//        System.out.println("I can calculate the factorial (!n) of any integer between 1 and 10. Try it now! Enter a number between 1 and 10: ");
+//        int userInput = sc.nextInt();
+//
+//        for(int n(n-1)
+
+
+//        if (userInput == 0) {
+//            System.out.println("Well I asked for a number between 1 and 10, but the factorial of 0 is: 1, but...");
+//            return calcFactorial(num);
+//        } else if (userInput > 10) {
+//            System.out.println("You really don't understand the assignment, do you? :/ well...");
+//            return calcFactorial(num);
+//        } else {
+
+//            }
+
+
+
+
+        }
+
+
+    public static void main(String[] args) {
+//        int addNums = add(3, 4);
+//        System.out.println(addNums);
+//
+//        int subtractNums = subtract(3, 7);
+//        System.out.println(subtractNums);
+//
+//        int multiplyNums = multiply(3, 4);
+//        System.out.println(multiplyNums);
+//
+//        int divideNums = divide(3, 12);
+//        System.out.println(divideNums);
+//
+//        int remainder = modulus(10, 2);
+//        System.out.println(remainder);
+
+
+//        getInteger(1, 10);
+
+//        calcFactorial();
+
+
+
+    }
+}
